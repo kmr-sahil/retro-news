ab8ebc04bf1023043c63ebf97163dd5ff6a37223
Sahil <sk2003311@gmail.com>
2023-08-25 14:18:41 +0530

    added the banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../index.css'
 
 export default function Navbar({change, setChange}) {
   const handleCountryChange = (event) => {
     setChange(event.target.value);
   };
   return (
-    <div className='flex flex-col my-[1rem]'>
-        <h2 className=' text-5xl text-[#393939] font-play font-bold mb-5'>Gotham News</h2>
+    <div className='flex flex-col my-[1rem] '>
+
+        <a target='_blank' href='https://github.com/kmr-sahil/retro-news#readme' className='bg-[#737373] w-[100%] absolute left-0 top-0'>
+          <p className='animate-marquee hover:pause hover:ease-in whitespace-nowrap text-[#e6e6e6] text-[12px] py-[2px]'> Free news API not available in production. Please run the code locally or <span className='underline underline-offset-2'> view the demo </span> ------- Free news API not available in production. Please run the code locally or <span className='underline underline-offset-2'> view the demo </span> ------- Free news API not available in production. Please run the code locally or <span className='underline underline-offset-2'> view the demo </span> ------- Free news API not available in production. Please run the code locally or <span className='underline underline-offset-2'> view the demo </span> ------- Free news API not available in production. Please run the code locally or <span className='underline underline-offset-2'> view the demo </span> Free news API not available in production. Please run the code locally or <span className='underline underline-offset-2'> view the demo </span> ------- Free news API not available in production. Please run the code locally or <span className='underline underline-offset-2'> view the demo </span> ------- Free news API not available in production. Please run the code locally or <span className='underline underline-offset-2'> view the demo </span> ------- Free news API not available in production. Please run the code locally or <span className='underline underline-offset-2'> view the demo </span> ------- Free news API not available in production. Please run the code locally or <span className='underline underline-offset-2'> view the demo </span></p>
+        </a>
+
+        <h2 className=' text-5xl text-[#393939] font-play font-bold mb-5 mt-4'>Gotham News</h2>
         <span className='w-[100%] h-[0.2em] bg-[#393939] rounded-md'/>
         
           <div className=' flex gap-x-5 gap-y-1 flex-wrap font-play text-[0.8rem] text-[#393939] underline my-[0.5rem]'>
